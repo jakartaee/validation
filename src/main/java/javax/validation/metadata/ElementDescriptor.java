a25ee24b844c6a103ebcb4642bc836723f78df26
Emmanuel Bernard <emmanuel@hibernate.org>
2009-10-03 15:39:37 +0000

    Make the API extensible for metadata navigation (ie restrict the list of ConstraintDescriptors).
    git-svn-id: https://svn.jboss.org/repos/hibernate/beanvalidation/api/trunk@17616 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/main/java/javax/validation/metadata/ElementDescriptor.java
+++ src/main/java/javax/validation/metadata/ElementDescriptor.java
@@ -18,6 +18,7 @@
 package javax.validation.metadata;
 
 import java.util.Set;
+import java.lang.annotation.ElementType;
 
 /**
  * Describes a validated element (class, field or property).
@@ -43,6 +44,7 @@ public interface ElementDescriptor {
 	 * empty <code>Set</code> if none are present.
 	 *
 	 * @return <code>Set</code> of constraint descriptors for this element
+	 * @deprecated {@link #findConstraints()}
 	 */
 	Set<ConstraintDescriptor<?>> getConstraintDescriptors();
 
@@ -56,6 +58,81 @@ public interface ElementDescriptor {
 	 *
 	 * @param groups groups targeted
 	 * @return list of matching ConstraintDescriptors
+	 * @deprecated {@link #findConstraints()}
 	 */
 	Set<ConstraintDescriptor<?>> getUnorderedConstraintDescriptorsMatchingGroups(Class<?>... groups);
+
+	/**
+	 * Find constraints and potentially restricting to certain criteria.
+	 *
+	 * @return ConstraintFinder object.
+	 */
+	ConstraintFinder findConstraints();
+
+	/**
+	 * Declare restrictions on how constraints are retrieved.
+	 * Restrictions are cumulative.
+	 *
+	 * A ConstraintFinder is not thread-safe. The set of matching
+	 * <code>ConstraintDescriptor</code> is.
+	 */
+	interface ConstraintFinder {
+		/**
+		 * Restrict to the constraints matching a given set of groups for this element
+		 *
+		 * This method respects group sequences and group inheritance (including
+		 * class-level <code>Default</code> group overriding) but does not return
+		 * <code>ConstraintDescriptor</code>s in any particular order.
+		 * Specifically, ordering of the group sequence is not respected.
+		 *
+		 * @param groups groups targeted
+		 *
+		 * @return <code>this</code> using the chaining method pattern
+		 */
+		ConstraintFinder unorderedAndMatchingGroups(Class<?>... groups);
+
+		/**
+		 * Restrict to the constraints matching the provided scope for this element.
+		 *
+		 * Defaults to <code>Scope.HIERARCHY</code>
+		 *
+		 * @param scope exected scope
+		 * @return <code>this</code> using the chaining method pattern
+		 */
+		ConstraintFinder lookingAt(Scope scope);
+
+		/**
+		 * Restrict to the constraints hosted on the listed <code>types</code>
+		 * for a given element.
+		 *
+		 * Default to all possible types of the element.
+		 *
+		 * Typically used to restrict to fields (<code>FIELD</code>)
+		 * or getters (<code>METHOD</code>)
+		 *
+		 * @param types targeted types
+		 * @return <code>this</code> using the chaining method pattern
+		 */
+		ConstraintFinder declaredOn(ElementType... types);
+
+		/**
+		 * Retrieve the constraint descriptors following the defined
+		 * restrictions and hosted on the element described by
+		 * <code>ElementDescriptor</code>
+		 *
+		 * @return matching constraint descriptors
+		 */
+		Set<ConstraintDescriptor<?>> getConstraintDescriptors();
+
+		/**
+		 * Returns <code>true</code> if at least one constraint declaration
+		 * matching the restrictions is present on the element,
+		 * <code>false</code> otherwise.
+		 *
+		 * @return is there any constraint
+		 */
+		boolean hasConstraints();
+
+	}
+
 }
