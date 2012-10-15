07afaa4e9a83a0bc95d12a2183b8d55b8569e611
Hardy Ferentschik <hardy@ferentschik.de>
2012-10-15 18:50:03 +0200

    BVAL-259 Enforce that evaluation fails on first error

--- src/main/java/javax/validation/ReportAsSingleViolation.java
+++ src/main/java/javax/validation/ReportAsSingleViolation.java
@@ -16,15 +16,22 @@
 */
 package javax.validation;
 
-import java.lang.annotation.Target;
 import java.lang.annotation.Retention;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * A constraint annotation hosting this annotation
- * will return the composed annotation error report if any of the composing annotations
- * fail. The error reports of each individual composing constraint is ignored.
+ * A constraint annotation hosting this annotation will return the
+ * composed annotation error report if any of the composing annotations fail.
+ * The error reports of each individual composing constraint are ignored.
+ *
+ * <p>
+ * Note: Evaluation of composed constraints stops on the first validation
+ * error in case the composing constraint is annotated with
+ * {@code @ReportAsSingleViolation}.
+ * </p>
  *
  * @author Emmanuel Bernard
  */
