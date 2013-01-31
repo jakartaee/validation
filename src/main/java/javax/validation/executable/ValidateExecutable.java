31ef5beefd94cf2a54cdd4d88ee462ca710c5d6b
Emmanuel Bernard <emmanuel@hibernate.org>
2013-01-31 13:24:55 +0100

    BVAL-327 Move executable objects to the executable package

--- src/main/java/javax/validation/executable/ValidateExecutable.java
+++ src/main/java/javax/validation/executable/ValidateExecutable.java
@@ -14,7 +14,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package javax.validation;
+package javax.validation.executable;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
