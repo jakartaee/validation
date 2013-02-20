283ed74b12b9e36b0e062bbfd1fc39b9e761dc3a
Emmanuel Bernard <emmanuel@hibernate.org>
2013-02-20 22:00:25 +0100

    BVAL-417 Rework @Constraint JavaDoc to mention how to write a constriaint

--- src/main/java/javax/validation/Constraint.java
+++ src/main/java/javax/validation/Constraint.java
@@ -16,6 +16,7 @@
 */
 package javax.validation;
 
+import javax.validation.constraintvalidation.SupportedValidationTarget;
 import javax.validation.constraintvalidation.ValidationTarget;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
@@ -25,10 +26,56 @@ import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Link between a constraint annotation and its constraint validation implementations.
+ * Marks an annotation as being a Bean Validation constraint.
  * <p/>
- * A given constraint annotation should be annotated by a {@code @Constraint}
+ * A given constraint annotation must be annotated by a {@code @Constraint}
  * annotation which refers to its list of constraint validation implementations.
+ * <p/>
+ * Each constraint annotation must host the following attributes:
+ * <ul>
+ *     <li>{@code String message() default [...];} which should default to an error
+ *     message key made of the fully-qualified class name of the constraint followed by
+ *     {@code .message}. For example {@code "{com.acme.constraints.NotSafe.message}"}</li>
+ *     <li>{@code Class<?>[] groups() default {};} for user to customize the targeted
+ *     groups</li>
+ *     <li>{@code Class<? extends Payload>[] payload() default {};} for
+ *     extensibility purposes</li>
+ * </ul>
+ * <p/>
+ * When building a constraint that is both generic and cross-parameter, the constraint
+ * annotation must host the {@code validationAppliesTo()} property.
+ * A constraint is generic if it targets the annotated element and is cross-parameter if
+ * it targets the array of parameters of a method or constructor.
+ * <pre>
+ *     ConstraintTarget validationAppliesTo() default ConstraintTarget.IMPLICIT;
+ * </pre>
+ * This property allows the constraint user to choose whether the constraint
+ * targets the return type of the executable or its array of parameters.
+ *
+ * A constraint is both generic and cross-parameter if
+ * <ul>
+ *     <li>two kinds of {@code ConstraintValidator}s are attached to the
+ *     constraint, one targeting {@link ValidationTarget.ANNOTATED_ELEMENT}
+ *     and one targeting {@link ValidationTarget.PARAMETERS},</li>
+ *     <li>or if a {@code ConstraintValidator} targets both
+ *     {@code ANNOTATED_ELEMENT} and
+ *     {@code PARAMETERS}.</li>
+ * </ul>
+ *
+ * Such dual constraints are rare. See {@link SupportedValidationTarget} for more info.
+ * <p/>
+ * Here is an example of constraint definition:
+ * <pre>
+ * &#64;Documented
+ * &#64;Constraint(validatedBy = OrderNumberValidator.class)
+ * &#64;Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+ * &#64;Retention(RUNTIME)
+ * public &#64;interface OrderNumber {
+ *     String message() default "{com.acme.constraint.OrderNumber.message}";
+ *     Class<?>[] groups() default {};
+ *     Class<? extends Payload>[] payload() default {};
+ * }
+ * </pre>
  *
  * @author Emmanuel Bernard
  * @author Gavin King
