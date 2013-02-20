22b6aab19f9d0076835569ae48b1364b0f8aab3e
Gunnar Morling <gunnar.morling@googlemail.com>
2013-02-20 13:51:46 +0100

    BVAL-414 Moving example listings to subpackage to separate them from real tests

--- src/test/java/javax/validation/examples/NodeTraversalTest.java
+++ src/test/java/javax/validation/examples/NodeTraversalTest.java
@@ -14,10 +14,11 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package javax.validation;
+package javax.validation.examples;
 
 import java.util.Iterator;
 
+import javax.validation.Path;
 import javax.validation.Path.*;
 
 /**
