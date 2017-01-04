7fda2bf1b9604b3444de2ba5601a9af8151a5092
Guillaume Smet <guillaume.smet@gmail.com>
2017-01-04 13:23:28 +0100

    BVAL-508 Add the API for value extraction

---
+++ src/main/java/javax/validation/valueextraction/ExtractedValue.java
@@ -0,0 +1,24 @@
+/*
+ * Bean Validation API
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package javax.validation.valueextraction;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation used to mark the type parameter to which a {@link ValueExtractor} is tied.
+ *
+ * @author Gunnar Morling
+ * @author Guillaume Smet
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE_USE)
+public @interface ExtractedValue {
+
+}
---
+++ src/main/java/javax/validation/valueextraction/ValueExtractor.java
@@ -0,0 +1,92 @@
+/*
+ * Bean Validation API
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package javax.validation.valueextraction;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.validation.Path;
+
+/**
+ * Defines the logic used to extract the values to validate from an object of type {@code T}.
+ * <p>
+ * A value extractor is tied to one specific type parameter of {@code T}. The {@link ExtractedValue} annotation
+ * is used to mark that type parameter.
+ * <p>
+ * The extracted values are passed to the corresponding method of the {@link ValueReceiver}.
+ * <p>
+ * A typical value extractor implementation for {@link List} may look like:
+ * <pre>
+ * public class ListValueExtractor implements ValueExtractor&lt;List&lt;&#064;ExtractedValue ?&gt;&gt; {
+ *
+ *     &#064;Override
+ *     public void extractValues(List&lt;?&gt; originalValue, ValueReceiver receiver) {
+ *         int i = 0;
+ *         for ( Object object : originalValue ) {
+ *             receiver.indexedValue( "&lt;collection element&gt;", i, object );
+ *             i++;
+ *         }
+ *     }
+ * }
+ * </pre>
+ *
+ * @author Gunnar Morling
+ * @author Guillaume Smet
+ */
+public interface ValueExtractor<T> {
+
+	/**
+	 * Extracts the values to validate from the original object.
+	 *
+	 * @param originalValue the original value from which we want to extract the values to validate
+	 * @param receiver the corresponding {@code ValueReceiver}
+	 */
+	void extractValues(T originalValue, ValueReceiver receiver);
+
+	/**
+	 * Provides a set of methods receiving value extracted by the {@link ValueExtractor}.
+	 * <p>
+	 * The value has to be passed to the method corresponding best to the type of the original value.
+	 */
+	interface ValueReceiver {
+
+		/**
+		 * Receives the value extracted from an object.
+		 *
+		 * @param nodeName the name of the node to be added to the {@link Path}
+		 * @param object the value to validate
+		 */
+		void value(String nodeName, Object object);
+
+		/**
+		 * Receives the value extracted from an {@link Iterable} object that is not indexed (e.g. a {@link Set}).
+		 *
+		 * @param nodeName the name of the node to be added to the {@link Path}
+		 * @param object the value to validate
+		 */
+		void iterableValue(String nodeName, Object object);
+
+		/**
+		 * Receives the value extracted from an indexed object (e.g. a {@link List} or an array).
+		 *
+		 * @param nodeName the name of the node to be added to the {@link Path}
+		 * @param i the index of the value in the original object
+		 * @param object the value to validate
+		 */
+		void indexedValue(String nodeName, int i, Object object);
+
+		/**
+		 * Receives the value extracted from a keyed object (e.g. a {@link Map}).
+		 *
+		 * @param nodeName the name of the node to be added to the {@link Path}
+		 * @param key the key of the value in the original object
+		 * @param object the value to validate
+		 */
+		void keyedValue(String nodeName, Object key, Object object);
+	}
+}
