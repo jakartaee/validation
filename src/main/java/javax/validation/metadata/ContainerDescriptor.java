71f7b6992bd5084357cbf3d693ef784aa5001d88
Gunnar Morling <gunnar.morling@googlemail.com>
2017-04-20 17:21:10 +0200

    BVAL-594 Adding missing license headers

--- src/main/java/javax/validation/metadata/ContainerDescriptor.java
+++ src/main/java/javax/validation/metadata/ContainerDescriptor.java
@@ -1,3 +1,9 @@
+/*
+ * Bean Validation API
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
 package javax.validation.metadata;
 
 import java.util.Set;
