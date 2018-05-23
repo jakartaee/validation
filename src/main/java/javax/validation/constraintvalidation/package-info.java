05c12609e2a255d954dc272d43b5b0b9cf220873
Gunnar Morling <gunnar.morling@googlemail.com>
2018-05-23 08:59:12 +0200

    BVAL-712 Typo fix

--- src/main/java/javax/validation/constraintvalidation/package-info.java
+++ src/main/java/javax/validation/constraintvalidation/package-info.java
@@ -5,7 +5,7 @@
  * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
  */
 /**
- * Package containing constructs specific to constraint validators
+ * Package containing constructs specific to constraint validators.
  * In particular {@link javax.validation.constraintvalidation.SupportedValidationTarget}
  * is used to refine the validation target of a {@link javax.validation.ConstraintValidator}.
  */
